637cec7d41127b94d4a99c1205ddbfd42bc77041
Amir Khamesy <amir@Amirs-MacBook-Air.local>
2023-08-22 13:43:38 -0700

    made modal close when user clicks outside or presses escape

--- app/components/Modal.jsx
+++ app/components/Modal.jsx
@@ -1,18 +1,42 @@
+import React, { useEffect } from "react";
+
 const Modal = ({ children, modalOpen, setModalOpen }) => {
+  useEffect(() => {
+    const handleCloseModal = (event) => {
+      if (event.key === "Escape") {
+        setModalOpen(false);
+      }
+    };
+
+    const handleClickOutsideModal = (event) => {
+      if (!event.target.closest(".modal-content")) {
+        setModalOpen(false);
+      }
+    };
+
+    if (modalOpen) {
+      document.addEventListener("keydown", handleCloseModal);
+      document.addEventListener("click", handleClickOutsideModal);
+    }
+
+    return () => {
+      document.removeEventListener("keydown", handleCloseModal);
+      document.removeEventListener("click", handleClickOutsideModal);
+    };
+  }, [modalOpen, setModalOpen]);
+
   return (
     <>
       {modalOpen && (
-        <div className="bg-black/50 fixed inset-0">
-          <div className="flex justify-center items-center h-full">
-            <div className="flex flex-col items-end bg-slate-300 w-1/2 p-5">
-              <button
-                onClick={() => setModalOpen(false)}
-                className="text-2xl mb-3"
-              >
-                &times; {/* Close Icon */}
-              </button>
-              {children}
-            </div>
+        <div className="fixed inset-0 flex justify-center items-center bg-black/50">
+          <div className="w-1/2 bg-slate-300 p-5 modal-content">
+            <button
+              onClick={() => setModalOpen(false)}
+              className="text-2xl mb-3"
+            >
+              &times; {/* Close Icon */}
+            </button>
+            {children}
           </div>
         </div>
       )}
