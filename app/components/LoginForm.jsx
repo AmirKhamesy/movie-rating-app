668f4568156d64ef4400fa21c76b623024fb01a4
Amir Khamesy <iknubs@gmail.com>
2023-08-31 15:52:51 -0700

    Login page responsiveness

--- app/components/LoginForm.jsx
+++ app/components/LoginForm.jsx
@@ -31,36 +31,34 @@ export default function LoginForm() {
   };
 
   return (
-    <div className="absolute h-screen w-screen bg-white top-0 left-0 pt-[50%]">
-      <div className="grid place-items-center">
-        <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
-          <h1 className="text-xl font-bold my-4">Login</h1>
+    <div className="absolute h-screen w-screen bg-white top-0 left-0 flex items-center justify-center ">
+      <div className="max-w-md w-full p-6 m-5 shadow-lg rounded-lg border-t-4 border-green-400">
+        <h1 className="text-xl font-bold my-4">Login</h1>
 
-          <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-            <input
-              onChange={(e) => setEmail(e.target.value)}
-              type="text"
-              placeholder="Email"
-            />
-            <input
-              onChange={(e) => setPassword(e.target.value)}
-              type="password"
-              placeholder="Password"
-            />
-            <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
-              Login
-            </button>
-            {error && (
-              <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
-                {error}
-              </div>
-            )}
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Password"
+          />
+          <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
+            Login
+          </button>
+          {error && (
+            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+              {error}
+            </div>
+          )}
 
-            <Link className="text-sm mt-3 text-right" href={"/register"}>
-              Don't have an account? <span className="underline">Register</span>
-            </Link>
-          </form>
-        </div>
+          <Link className="text-sm mt-3 text-right" href={"/register"}>
+            Don't have an account? <span className="underline">Register</span>
+          </Link>
+        </form>
       </div>
     </div>
   );
