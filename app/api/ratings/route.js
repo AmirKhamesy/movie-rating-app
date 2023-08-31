6ee142abb4a7b5f102760e28cd35b6cb5d4d4a50
Amir Khamesy <iknubs@gmail.com>
2023-08-31 15:38:10 -0700

    Protecting sensitive endpoints in server

--- app/api/ratings/route.js
+++ app/api/ratings/route.js
@@ -1,9 +1,19 @@
 //http://localhost:3000/api/ratings
 import prisma from "@/app/libs/prismadb";
 import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 export const POST = async (req) => {
   try {
+    //Check if user is auth
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return new NextResponse(JSON.stringify({ error: "unauthorized" }), {
+        status: 401,
+      });
+    }
+
     const body = await req.json();
     const { title, scary, story, acting } = body;
 
@@ -25,11 +35,18 @@ export const POST = async (req) => {
 
 export const GET = async () => {
   try {
+    //Check if user is auth
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return new NextResponse(JSON.stringify({ error: "unauthorized" }), {
+        status: 401,
+      });
+    }
+
     const rating = await prisma.rating.findMany();
 
     return NextResponse.json(rating);
   } catch (error) {
-    console.log(error);
     return NextResponse.json({ message: "GET Error" }, { status: 500 });
   }
 };
--- app/rate/page.jsx
+++ app/rate/page.jsx
@@ -1,16 +1,12 @@
-import { getServerSession } from "next-auth";
 import AddRating from "../components/AddRating";
 import RatingsList from "../components/RatingsList";
-import { authOptions } from "../api/auth/[...nextauth]/route";
-import { redirect } from "next/navigation";
-
-export const fetchCache = "force-no-store";
-export const revalidate = 0; // seconds
-export const dynamic = "force-dynamic";
+import { cookies } from "next/headers";
 
 async function getRatings() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API}/api/ratings`, {
     next: { revalidate: 0 },
+    credentials: "include",
+    headers: { Cookie: cookies().toString() }, //HACK: Send cookies to server
   });
 
   if (!res.ok) {
@@ -22,9 +18,6 @@ async function getRatings() {
 
 const RatePage = async () => {
   const ratings = await getRatings();
-  const session = await getServerSession(authOptions);
-
-  if (!session) redirect("/");
 
   return (
     <div className="max-w-4xl mx-auto mt-4">
