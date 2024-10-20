3fe14bf7d7b19c04ba46a0c4b300174a4514da5a
Amir Khamesy <iknubs@gmail.com>
2024-10-20 00:54:31 -0700

    adding home page

--- app/page.js
+++ app/page.js
@@ -1,17 +1,25 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
-import { LogoutButton } from "./auth";
 import LoginForm from "./components/LoginForm";
+import HomeContent from "./components/HomeContent";
 
 const HomePage = async () => {
   const session = await getServerSession(authOptions);
 
   if (!session) return <LoginForm />;
 
+  const publicLists = await fetch(
+    `${process.env.NEXT_PUBLIC_API}/api/publicList`,
+    {
+      cache: "no-store",
+    }
+  ).then((res) => res.json());
+
   return (
-    <div className="flex justify-between">
-      <h1 className="text-4xl">Hello {session?.user?.name}</h1>
-      <LogoutButton />
+    <div className="min-h-screen bg-white py-12">
+      <div className="container mx-auto px-4">
+        <HomeContent userName={session.user.name} publicLists={publicLists} />
+      </div>
     </div>
   );
 };
